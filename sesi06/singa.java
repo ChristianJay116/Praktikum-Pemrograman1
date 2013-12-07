d451cf8cd95ff17c8f97fd4e5184418ee6fe0dcc
unknown <christian.jay116@gmail.com>
2013-12-07 20:42:40 +0700

    commit01

---
+++ sesi06/singa.java
@@ -0,0 +1,12 @@
+class singa extends  kucingbesar
+{
+	public singa(String namasinga)
+	{
+		singa.nama = namasinga;
+	}
+	public void makan()
+	{
+		System.out.println("Singa makan daging");
+		System.out.println();
+	}
+}
